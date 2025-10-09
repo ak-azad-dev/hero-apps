328d00dc93b54b2c050c0a20b1d0993e712e6941
Abul Kalam Azad <azadsmc@gmail.com>
2025-10-09 07:03:43 +0600

    Added footer

--- src/Components/Apps/Apps.jsx
+++ src/Components/Apps/Apps.jsx
@@ -5,7 +5,7 @@ import AppCard from "../AppCard/AppCard";
 const Apps = ({ apps }) => {
   const filteredApps = apps.filter((app) => app.id <= 8)
   return (
-    <div className="mt-[80px] mb-[168px] px-10 w-full md:max-w-[1440px] mx-auto items-center">
+    <div className="mt-[80px] mb-[80px] px-10 w-full md:max-w-[1440px] mx-auto items-center">
       <h1 className="text-center text-[48px] font-bold text-[#001931]">
         Trending Apps
       </h1>
@@ -17,7 +17,7 @@ const Apps = ({ apps }) => {
           <AppCard key={app.id} app={app} />
         ))}
       </div>
-      <div className="flex justify-center mt-[40px]">
+      <div className="flex justify-center mt-[50px]">
         <a
           className="btn text-base shadow-none text-white font-semibold bg-linear-to-r from-[#632EE3] to-[#9F62F2]
           border-0 flex gap-2.5 rounded-[4px] px-4 py-3 transition duration-300 ease-in transform hover:scale-105"
